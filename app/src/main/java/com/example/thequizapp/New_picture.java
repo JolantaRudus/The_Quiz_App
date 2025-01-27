adc613b7cb3877a33750ce356bdc5a62b099e709
OdaBStorebo <oda.bastesen.storebo@gmail.com>
2025-01-27 12:51:02 +0100

    Added New_picture veiw. Buttons have no function yet

--- app/src/main/java/com/example/thequizapp/New_picture.java
+++ app/src/main/java/com/example/thequizapp/New_picture.java
@@ -1,6 +1,11 @@
 package com.example.thequizapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
