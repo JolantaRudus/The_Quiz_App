427ab31a0a4b8d2c6ab65cbf2bd1d8dcac81edf9
669834 <anilabgs@gmail.com>
2025-01-29 00:06:03 +0100

    Photos to the galery (15 photos) ImageItem class GalleryAdapter class gallery_item xml gallery_main xml Conected Qiuz button to Quiz.class Added some new images to the list Added sort method to button1 (A-z or Z-A)
    Added delete button to the cardView, in gallery_item-xml
    Added delete method in GalleryAdapter.java and updated the Gallery.java class

--- app/src/main/java/com/example/thequizapp/GalleryAdapter.java
+++ app/src/main/java/com/example/thequizapp/GalleryAdapter.java
@@ -1,8 +1,12 @@
 package com.example.thequizapp;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,19 +18,23 @@ import java.util.List;
 public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.GalleryViewHolder> {
 
     private List<ImageItem> imageList;
+    private Context context;
 
-    public GalleryAdapter(List<ImageItem> imageList) {
+    public GalleryAdapter(Context context, List<ImageItem> imageList) {
+        this.context = context; // Initialize the context
         this.imageList = imageList;
     }
 
     public static class GalleryViewHolder extends RecyclerView.ViewHolder {
         public ImageView imageView;
         public TextView titleTextView;
+        public ImageButton deleteButton;
 
         public GalleryViewHolder(View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.cardImageView);
             titleTextView = itemView.findViewById(R.id.cardTitleTextView);
+            deleteButton =itemView.findViewById(R.id.deleteButton);
         }
     }
 
@@ -42,10 +50,37 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.GalleryV
         ImageItem currentItem = imageList.get(position);
         holder.imageView.setImageResource(currentItem.getImageResId());
         holder.titleTextView.setText(currentItem.getTitle());
+        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDeleteConfirmationDialog(holder.getAdapterPosition());
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return imageList.size();
+    }
+
+    //Method to show the delete image confirmation dialog
+    private void showDeleteConfirmationDialog(int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Delete Image");
+        builder.setMessage("Are you sure you want to delete this image?");
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                deleteImage(position);
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+        builder.show();
+    }
+
+    //Method to delete the image from the list
+    private void deleteImage(int position) {
+        imageList.remove(position);
+        notifyItemRemoved(position);
     }
 }
